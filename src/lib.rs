99dd0b539dcbb76fd3a5d68bd2b7ec0417f31b0b
Eric Grochowski <egrochowski42@gmail.com>
2023-12-03 20:52:55 -0500

    Day three

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,10 @@ pub fn parse_input(file: &str) -> String {
     let path = cwd.join("inputs").join(file); 
     fs::read_to_string(path).unwrap()
 }
+
+pub fn parse_to_matrix(file: &str) -> Vec<Vec<char>> {
+    parse_input(file)
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect()
+}   
